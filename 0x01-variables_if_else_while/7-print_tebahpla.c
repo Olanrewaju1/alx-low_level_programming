0327f221e73721c71f5a07ce715b892541ff3029
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-08 19:52:54 -0700

    program that print lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function to print lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar(c);
+	return (0);
+}
