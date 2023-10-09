9c49ed5fdcc6616abfc7c5b8da48b8d868a3c4e5
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-08 19:31:42 -0700

    function to print lowercase and omit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function thta print lowercase except q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
