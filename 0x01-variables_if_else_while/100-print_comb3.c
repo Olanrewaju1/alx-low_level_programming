8442575b51ea870cbbe827b9efaa360480fc6c9f
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-09 04:38:41 -0700

    Program to print 2 digit with no repetition

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,26 +11,23 @@ int main(void)
 	int i = 0;
 	int j = 1;
 
-	for (; i <= 8;)
+	while (i < 9)
 	{
-		for (; j <= 9;)
-                {
+		while (j < 10)
+		{
 			putchar(i + 48);
-                        putchar(j + 48);
-                        if (i != 8 || j != 9)
-                        {
-                                putchar(44);
-                                putchar(32);
-                        }
-
+			putchar(j + 48);
+			if (i + 48 < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 			j++;
+		}
 
-
-                }
-		i++
-                j = i + 1;
-        }
-
-        putchar(10);
-        return (0);
+		i++;
+		j = i + 1;
+	}
+	putchar(10);
+	return (0);
 }
