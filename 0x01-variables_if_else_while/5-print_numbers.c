7185c81a5944e05ba03aff1bcf03ca58eae2cb9b
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-08 19:42:36 -0700

    single digit of base 10'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function to print all single digit of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar(10);
+	return (0);
+}
