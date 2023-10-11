7371f4905c1d06742a2b9622456f0247232433bb
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-11 10:12:28 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function to prints the sign of a number
+ *
+ * @n: number
+ * Return:1 0r 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 1)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
