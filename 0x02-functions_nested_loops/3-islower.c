010813061b302079f494dc303e8833879ba0b721
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-11 05:59:15 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if a character is lowercase
+ *
+ * Return: 1
+ */
+
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'A'; i <= 'z'; i++)
+	{
+		if (i >= 'a' && i <= 'z')
+		{
+			return (1);
+		}
+
+		else
+			return (0);
+	}
+}
