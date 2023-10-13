3307a1cfb3254c2155fbc9695d61e1629cf81a1a
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-13 07:18:56 -0700

    Function to print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ * n: variable
+ *
+ * Return:0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(n);
+	}
+
+	else
+		_putchar(n);
+
+	return (0);
+}
