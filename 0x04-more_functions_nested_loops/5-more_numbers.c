db496d6f72805db3e237fb58345560dbe4675be3
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-14 05:54:55 -0700

    function to print 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print 0 -14
+ *
+ * Return:void
+ */
+
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar(i % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
