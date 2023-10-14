869ff0d4faf28ae1d9346435ec0f21f443534d99
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-14 04:00:15 -0700

    function to multiply two integer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 
 /**
  * mul - function to multiply two integers
- * @a-first int parameter
+ * @a:first int parameter
  * @b:second integer parameter
  * Return:0(Successful)
  */
@@ -10,8 +10,7 @@
 
 int mul(int a, int b)
 {
-	int num;
-	num = a * b;
-	
+	int num = a * b;
+
 	return (num);
 }
