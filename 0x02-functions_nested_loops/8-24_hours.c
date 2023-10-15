4789e1c36ebf614a7cb7119739da0e9dec6a1674
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-15 08:35:37 -0700

    function to print 9 times

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function thts print every minute of a day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table starting with 0
+ *
+ * Return: void
+ */
+
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int num == i * j;
+
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+			}
+
+			_putchar(num % 10 + '0');
+			if (j != 9)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+
+		}
+
+		_putchar('\n');
+	}
+}
