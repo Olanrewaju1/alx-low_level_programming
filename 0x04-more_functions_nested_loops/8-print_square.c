9240890372aa1f50ae9de7352f946755e3906618
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-15 17:01:58 -0700

    function to print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - function to print square based on input
+ * @size:parameter
+ *
+ * Return: 0
+ */
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+
+			if (size < 0)
+			{
+				_putchar('\n');
+				break;
+			}
+		}
+
+		_putchar('\n');
+	}
+
+	return (0);
+}
