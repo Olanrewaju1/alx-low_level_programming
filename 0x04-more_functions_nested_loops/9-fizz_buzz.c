3ae295a5dd95c9cee7333aa52076a1ccd86b677d
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-15 17:29:46 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,28 +11,26 @@ int main(void)
 {
 	int i;
 
-	putchar(1 + '0');
-
 	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
 
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
 
 		else
 		{
-			printf(" %d", i);
+			printf("%d ", i);
 		}
 
 	}
