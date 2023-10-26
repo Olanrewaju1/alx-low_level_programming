37c4c59b2ce480f4d5b82f229a31ae9189318c14
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-10-26 11:18:39 -0700

    README.md

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print the highest prime factor
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - function to print integer
+ * @n:paramter
+ *
+ * Return:void
+ */
+
+void print_number(int n)
+{
+	if (n > 0)
+	{
+		_putchar(
+	}
+}
