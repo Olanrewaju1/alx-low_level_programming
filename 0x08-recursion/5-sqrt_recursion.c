68af2597c400a5967831b70c12b32f93666cdc08
Toheeb Olanrewaju <toheebolawale019@gmail.com>
2023-11-04 07:03:54 -0700

    function to return square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,36 @@
 #include "main.h"
 
 /**
+ * _sqr_pre - function to return the natural square root
+ * @n: first int
+ * @guess: first guess
+ *
+ * Return: 0
+ */
+
+int _sqr_pre(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (n);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqr_pre(n, guess + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - function to return the square root of a integer with
  * natural square root
  * @n: integer
  *
  * Return:0(Always)
  */
-
 
 int _sqrt_recursion(int n)
 {
@@ -16,13 +39,13 @@ int _sqrt_recursion(int n)
 		return (-1);
 	}
 
-	else if(n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 	{
 		return (1);
 	}
 
 	else
 	{
-		return _sqr_pre(n, 1);
+		return (_sqr_pre(n, 1));
 	}
 }
