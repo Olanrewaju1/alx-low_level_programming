4fab09bae9030072707cb3fb7db8bd4bd3e2fa02
Olanrewaju1 <toheebolawale019@gmail.com>
2023-11-21 19:56:42 +0100

    function to convert string to Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to convert string to uppercase
+ * @s : string to be converted
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/tester.c
+++ 0x06-pointers_arrays_strings/tester.c
@@ -11,6 +11,6 @@ int main(void)
     
     printf("%d\n", result);
     printf("%d\n", strcmp(s2, s1));
-    printf("%d\n", strcmp(s1, s1));
+    printf("%d\n", 7 / 2);
     return (0);
 }
