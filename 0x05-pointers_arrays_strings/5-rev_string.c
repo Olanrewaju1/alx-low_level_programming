2fddb61e4dee99323b96e672d1f27888b7813866
Olanrewaju1 <toheebolawale019@gmail.com>
2024-01-28 14:12:15 +0100

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,12 +15,12 @@ void rev_string(char *s)
 
 	if (s == NULL)
 		return;
-	while(s[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (i = 0, j = length - 1; i < j; i++,j--)
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
 		str = s[i];
 		s[i] = s[j];
